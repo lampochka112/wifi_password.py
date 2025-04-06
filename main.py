8eac5793d6b822f2a71f34f08cd52c003bac7de7
bobrdobr1z <bobrdobr112zxc@bk.ru>
2025-04-06 18:54:20 +0300

    friends don't ban me

--- main.py
+++ main.py
@@ -1,32 +1,26 @@
 import subprocess
 
-data = subprocess.check_output(['netsh', 'wlan', 'show',
-'profiles']).decode('utf-8', errors="backslashreplace").split(
-      '\n')
+data = subprocess.check_output(['netsh', 'wlan', 'show', 'profiles']).decode('utf-8', errors="backslashreplace").split('\n')
 
 profile = []
 for i in data:
-    if "All User profile" in i:
-        profile.append(i.split(":")[1] [1 : -1])
+    if "All User Profile" in i:  
+        profile.append(i.split(":")[1].strip())
 
 for i in profile:
     try:
-        results = subprocess.check_output(['netsh', 'wlan', 'show',
-'profiles', i, "key=clear"]).decode('utf-8'
-        
-errors="backslashreplace").split(
-    '\n')
+        results = subprocess.check_output(['netsh', 'wlan', 'show', 'profiles', i, "key=clear"]).decode('utf-8', errors="backslashreplace").split('\n')
         result = []
 
-for b in results:
-    if "key Content" in b:
-        result.append(b.split(":") [1] [1 : -1])
+        for b in results:
+            if "Key Content" in b:  
+                result.append(b.split(":")[1].strip())  
 
-try: 
-    print("{:<30} | {:<}".format(i, result[0]))
-except Exception as e:
-     print("{:<30} | {:<}".format(i,""))
-except Exception as e:
-     print("{:<30} | {:<}".format(i, "ERROR OCCURED"))
+        if result:
+            print("{:<30} | {:<}".format(i, result[0]))
+        else:
+            print("{:<30} | {:<}".format(i, "No password set"))
+
+    except subprocess.CalledProcessError:
+        print("{:<30} | {:<}".format(i, "ERROR OCCURRED"))
      
-#friends don't ban me
